caf0e19f7618aa1572917d2481bbb5402714abdf
Coby Welty <cobywelty98@gmail.com>
2024-08-07 03:41:31 +0000

    finished the exercise prep operators and expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,50 @@
+let number1;
+let number2;
+
+number1 = 10;
+number2 = 20;
+
+let product = number1 * number2;
+
+console.log(product);
+console.log(typeof product);
+
+let charge = 10;
+let payment = 50;
+let amountRemaining = payment - charge;
+
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+let tests = 90;
+let assignments = 85;
+let final = 95;
+let grade = (tests + assignments + final) / 3;
+
+console.log(grade);
+console.log(typeof grade);
+
+let firstName = 'Coby';
+let lastName = 'Welty';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(typeof fullName);
+
+let pH = 12;
+let isAcidic = pH < 7;
+
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+let numberOfSoldiers = 300;
+let isSparta = numberOfSoldiers === 300;
+
+console.log(isSparta);
+console.log(typeof isSparta);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+
+console.log(nickname);
+console.log(typeof nickname);
